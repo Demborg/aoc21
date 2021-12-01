c6bcfda341a0ae2962afe9cace1afa7221d0333a
Axel Demborg <axel.demborg@gmail.com>
2021-12-01 17:16:23 +0100

    Refactor: breakout printIncreaseCount

--- 1.swift
+++ 1.swift
@@ -1,19 +1,21 @@
+func printIncreaseCount(in data: Array<Int>) {
+    print(
+        zip(data, data[1...])
+            .filter { $1 > $0 }
+            .count
+    )
+}
+
 var data: Array<Int> = []
 while let rawValue = readLine(strippingNewline: true) {
     if let value = Int(rawValue) {
         data.append(value)
     }
 }
-print(
-    zip(data, data[1...])
-        .filter { $1 > $0 }
-        .count
-)
+
+printIncreaseCount(in: data)
 
 let smooth = zip(data, zip(data[1...], data[2...]))
     .map { $0 + $1.0 + $1.1 }
-print(
-    zip(smooth, smooth[1...])
-        .filter { $1 > $0 }
-        .count
-)
+
+printIncreaseCount(in: smooth)

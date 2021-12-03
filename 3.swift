d32150ef55869e3a2d4bc1d4914a0643d22df7b9
Axel Demborg <axel.demborg@gmail.com>
2021-12-03 20:35:55 +0100

    Solve day 3 part 2

--- 3.swift
+++ 3.swift
@@ -1,8 +1,19 @@
 import Foundation
 
-var data: Array<Array<Float>> = []
+typealias Row = Array<Float>
+
+func float(from row: Row) -> Float {
+    row
+        .reversed()
+        .enumerated()
+        .reduce(0) {
+            $0 + NSDecimalNumber(decimal: pow(2, $1.0)).floatValue * round($1.1)
+        }
+}
+
+var data: Array<Row> = []
 while let rawValue = readLine(strippingNewline: true) {
-    var line: Array<Float> = []
+    var line: Row = []
     for char in rawValue {
         line.append(char == "1" ? 1 : 0)
     }
@@ -14,8 +25,20 @@ let means = data
         zip(counts, line).map { $0 + $1 }
     }
     .map { $0 / Float(data.count) }
-print(means)
-let gamma = means.reversed().enumerated().reduce(0) {$0 + NSDecimalNumber(decimal: pow(2, $1.0)).floatValue * round($1.1)}
+let gamma = float(from: means)
 let epsilon = NSDecimalNumber(decimal: pow(2, data[0].count)).floatValue - 1 - gamma
-print(gamma, epsilon)
 print(gamma * epsilon)
+
+func getRating(in data: Array<Row>, max: Bool, index: Int = 0) -> Row {
+    if data.count == 1 {
+        return data[0]
+    }
+    let mostCommon = round(data.reduce(0) { $0 + $1[index] } / Float(data.count))
+    return getRating(
+        in: data.filter { $0[index] == (max ? mostCommon : 1 - mostCommon) },
+        max: max,
+        index: index+1
+    )
+}
+
+print([true, false].map { float(from: getRating(in: data, max: $0)) }.reduce(1, *))

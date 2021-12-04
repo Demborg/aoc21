a323520787c5d55fc8c7f32cec6a05e7f1eefe45
Axel Demborg <axel.demborg@gmail.com>
2021-12-04 13:44:09 +0100

    Day 4, Finding the winning boards

---
+++ 4.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let input = readLine()!.split(separator: ",").map {Int($0)!} 
+
+var boardStrings = ""
+while let line = readLine(strippingNewline: false) {
+    boardStrings += line
+}
+
+let boards = boardStrings
+    .components(separatedBy: "\n\n")
+    .map { board in 
+        board
+            .split(separator: "\n")
+            .map { row in
+                row
+                    .split(separator: " ")
+                    .map {Int($0)!}}
+    }
+
+print(
+    (0...input.count-1)
+        .map {input[0...$0]}
+        .map {partial in 
+            boards.filter { board in 
+                board.reduce(false) {win, row in win || row.count == row.filter {partial.contains($0)}.count}
+                || (0...board.count-1).reduce(false) {win, index in win || board.count == board.map {row in row[index]}.filter {partial.contains($0)}.count}
+            }
+        }
+)

41027700bb304eee0ad45d4f5b5a63a8a48d8ae1
Axel Demborg <axel.demborg@gmail.com>
2021-12-04 14:56:32 +0100

    Day4 part 2

--- 4.swift
+++ 4.swift
@@ -21,11 +21,18 @@ let boards = boardStrings
 let winningBoards = (0...input.count-1)
     .map {input[0...$0]}
     .map {partial in 
-        boards.filter { board in 
+        boards.enumerated().filter { _, board in 
             board.reduce(false) {win, row in win || row.count == row.filter {partial.contains($0)}.count}
             || (0...board.count-1).reduce(false) {win, index in win || board.count == board.map {row in row[index]}.filter {partial.contains($0)}.count}
-        }.map { ($0, partial) }
+        }.map { ($0.0, $0.1, partial) }
     }
     .filter {$0.count > 0}
 
-print(winningBoards.first!.map {board, partial in board.reduce(0) { $0 + $1.filter { !partial.contains($0) }.reduce(0, +) } * partial.last! })
+let winningPoints = winningBoards
+    .map {
+        $0.map {index, board, partial in (index, board.reduce(0) { $0 + $1.filter { !partial.contains($0) }.reduce(0, +) } * partial.last!) }
+    }
+
+print(winningPoints.first!.first!.1)
+
+print(winningPoints.reduce((0, [])) {old, new in (new.first {!old.1.contains($0.0)}?.1 ?? old.0, new.map {$0.0})} )

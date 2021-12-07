451d378901afecb01cf09c420072f9383575c5e2
Axel Demborg <axel.demborg@gmail.com>
2021-12-07 07:31:42 +0100

    day7 part 2

--- 7.swift
+++ 7.swift
@@ -4,3 +4,14 @@ print(
         .map {crabs.reduce(($0, 0)) { ($0.0, $0.1 + abs($1 - $0.0)) }.1 }
         .min()!
 )
+
+func moveCost(_ start: Int, _ stop: Int) -> Int {
+    let absDiff = abs(start - stop)
+    return absDiff * (absDiff + 1) / 2
+}
+
+print(
+    (0...crabs.max()!)
+        .map {crabs.reduce(($0, 0)) { ($0.0, $0.1 + moveCost($1, $0.0)) }.1 }
+        .min()!
+)

34f521c2e7a990cfc75847873db19f2890d4c50c
Axel Demborg <axel.demborg@gmail.com>
2021-12-09 19:54:29 +0100

    Day9 part 2, really nasty though

--- 9.swift
+++ 9.swift
@@ -24,5 +24,37 @@ for row in heightMap.enumerated() {
         }
     }
 }
-
 print(lowPoints.reduce(0) {$0 + $1.value + 1})
+
+var lakes: [Int] = []
+for point in lowPoints {
+    var ridges = heightMap.map {$0.map { value -> Bool? in value == 9 ? nil : false}}
+    ridges[point.y][point.x] = true
+    var count = 0
+    while (count != ridges.reduce(0) {$0 + $1.reduce(0) {$0 + ($1 ?? false ? 1 : 0)}}) {
+        count = ridges.reduce(0) {$0 + $1.reduce(0) {$0 + ($1 ?? false ? 1 : 0)}}
+        for row in ridges.enumerated() {
+            let y = row.0
+            let line = row.1
+            for cell in line.enumerated() {
+                let x = cell.0
+                let element = cell.1
+                var member = false
+                for offsets in [(y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)] {
+                    let xi = offsets.1
+                    let yi = offsets.0
+                    if (0..<line.count).contains(xi) && (0..<heightMap.count).contains(yi){
+                        if let other = ridges[yi][xi], let _ = element {
+                            member = member || other
+                        }
+                    }
+                }
+                if member {
+                    ridges[y][x] = true
+                }
+            }
+        }
+    }
+    lakes.append(count)
+}
+print(lakes.sorted().suffix(3).reduce(1, *))

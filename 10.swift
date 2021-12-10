5a94e3c9f5eb5eae292c12158388fe2593550c5c
Axel Demborg <axel.demborg@volumental.com>
2021-12-10 16:24:10 +0100

    Day10 part2

--- 10.swift
+++ 10.swift
@@ -1,12 +1,12 @@
 let closing: [Character: Character] = ["(": ")", "[": "]", "{": "}", "<": ">"]
-let scores: [Character: Int] = [")": 3, "]": 57, "}": 1197, ">": 25137]
+let scores: [Character: (Int, Int)] = [")": (3, 1), "]": (57, 2), "}": (1197, 3), ">": (25137, 4)]
 
 var lines: [String] = []
 while let line = readLine() {
     lines.append(line)
 }
 
-let fails = lines.compactMap {line -> Character? in
+let fails = lines.map {line -> (Int?, Int?) in
     var thing: [Character] = []
     for c in line {
         if "([{<".contains(c) {
@@ -14,10 +14,12 @@ let fails = lines.compactMap {line -> Character? in
         } else if closing[thing.last!]! == c {
             thing.removeLast()
         } else {
-            return c
+            return (scores[c]?.0, nil)
         }
     }
-    return nil
+    return (nil, thing.map{scores[closing[$0]!]!.1}.reversed().reduce(0) {5 * $0 + $1})
 }
 
-print(fails.reduce(0) { $0 + scores[$1]! })
+print(fails.compactMap{$0.0}.reduce(0,+))
+let completionScores = fails.compactMap{$0.1}.sorted()
+print(completionScores[completionScores.count / 2])

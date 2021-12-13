57a76b9dc50f2fa1402401eec0bf4c369cac1b74
Axel Demborg <axel.demborg@gmail.com>
2021-12-13 08:00:50 +0100

    Day 13 part 1

---
+++ 13.swift
@@ -0,0 +1,26 @@
+var dots: [[Int]] = []
+var folds: [(dir: Int, cord: Int)] = []
+
+while let line = readLine() {
+    if line.count == 0 {
+        continue
+    }
+    if line.first! == "f" {
+        let fold = line.dropFirst("fold along ".count).split(separator: "=")
+        folds.append((dir: fold[0] == "x" ? 0 : 1, Int(fold[1])!))
+    } else if line.count > 0 {
+        dots.append(line.split(separator: ",").map {Int($0)!})
+    }
+}
+
+let fold = folds.first!
+print(
+    Dictionary(
+        grouping: dots.map {dot in
+            dot.enumerated().map {
+                $0.0 == fold.dir ? ($0.1 > fold.cord ? 2 * fold.cord - $0.1 : $0.1): $0.1
+            } 
+        },
+        by: {$0}
+    ).count
+)
